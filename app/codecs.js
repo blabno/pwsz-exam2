02b51c3e43a1ef2549f82a1e271cf7f7a3b68427
woj1993 <woj_93@tlen.pl>
2015-05-20 10:41:00 +0200

    js

--- app/codecs.js
+++ app/codecs.js
@@ -1,5 +1,9 @@
+/* exported codecA,codecB,codecC,codecD */
+
 function codecA(a, b)
 {
+    'use strict';
+    /*jshint bitwise: false*/
     if (a & b) {
         return a + b;
     } else {
@@ -8,7 +12,8 @@ function codecA(a, b)
 }
 function codecB(a, b)
 {
-    if (a & b) {
+    'use strict';
+    if (a && b) {
         return a + b;
     } else {
         return a + ',' + b;
@@ -16,9 +21,12 @@ function codecB(a, b)
 }
 function codecC(a, b)
 {
+    'use strict';
+    /*jshint eqeqeq: false*/
     return a == b;
 }
 function codecD(a, b)
 {
-    return a == b;
+    'use strict';
+    return a === b;
 }
