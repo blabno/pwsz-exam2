8b520915c62f7eafe6f50362d2b8bfa4dba8f1e5
Tomasz Bieńkowski <tomasz@bienkowski.it>
2015-05-20 11:05:06 +0200

    done

--- spec/codecs.spec.js
+++ spec/codecs.spec.js
@@ -1,6 +1,7 @@
 /*globals codecA, codecB, codecD*/
 describe('codecA', function ()
 {
+    'use strict';
     describe('when a and b do the magic together', function ()
     {
         it('should return their sum', function ()
@@ -23,6 +24,7 @@ describe('codecA', function ()
 
 describe('codecB', function ()
 {
+    'use strict';
     describe('when a and b do the magic together', function ()
         {
             it('should return their sum', function ()
@@ -45,6 +47,7 @@ describe('codecB', function ()
 
 describe('codecC', function ()
 {
+    'use strict';
     describe('when a and b do the magic together', function ()
     {
         it('should return true', function ()
@@ -68,6 +71,8 @@ describe('codecC', function ()
 
 describe('codecD', function ()
 {
+    'use strict';
+    /* global codecC:false */
     describe('when a and b do the magic together', function ()
     {
         it('should return true', function ()
