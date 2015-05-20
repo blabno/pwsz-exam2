00c2bccfdd74236d8bd3f13c07411f5854e7b194
woj1993 <woj_93@tlen.pl>
2015-05-20 11:12:18 +0200

    c

--- src/test/java/pl/labno/bernard/TerminalTest.java
+++ src/test/java/pl/labno/bernard/TerminalTest.java
@@ -1,5 +1,36 @@
 package pl.labno.bernard;
 
-public class TerminalTest {
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class TerminalTest {
+    @Rule
+    public ExpectedException expectedException= ExpectedException.none();
+
+    @Test
+    public void sendLine_null_argument()
+    {
+        Connection connectionMock = Mockito.mock(Connection.class);
+        Terminal terminal = new Terminal(connectionMock);
+        expectedException.expect(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void send_line_unknownCommandException()
+    {
+        Connection connectionMock = Mockito.mock(Connection.class);
+        Terminal terminal = new Terminal(connectionMock);
+
+        when(connectionMock.isConnected()).thenReturn(true);
+
+        terminal.sendLine("line");
+        String errorMsg = terminal.getErrorMessage();
+
+        assertEquals("This command is unknown", errorMsg);
+    }
 }
