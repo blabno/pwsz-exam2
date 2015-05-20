5dcc1f33583fe961ae96bce69930f1bf66a37dea
Woj_93 <woj_93@tlen.pl>
2015-05-20 11:14:38 +0200

    Testy UnknowCommandExeption.

---
+++ src/test/java/pl/labno/bernard/UnknowCommandExceptionTest.java
@@ -0,0 +1,22 @@
+package pl.labno.bernard;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.internal.matchers.Null;
+
+/**
+ * Created by student on 20.05.15.
+ */
+
+public class UnknowCommandExceptionTest {
+    @Test
+    public void creating_test() {
+        // Given
+        UnknownCommandException a = new UnknownCommandException();
+        // Then
+        // When
+    }
+}
